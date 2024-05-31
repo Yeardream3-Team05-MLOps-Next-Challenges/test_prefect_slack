74c918ddabbceb22ecf70f4c9a19dfe99478c495
sanghun9634 <sanghun9634@gmail.com>
2024-05-31 20:29:07 +0900

    test: deploy local docker

--- flow.py
+++ flow.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             name="test-prefect-docker",
             tag="0.2",
             #buildargs={"PYTHON_VERSION": "3.10"},
-            platform="linux/arm64"
+            platform="linux/amd64"
         ),
         cron="0 * * * *",
     )
