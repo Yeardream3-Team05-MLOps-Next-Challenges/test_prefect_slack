231c0d104d1b71b3ec999f42d433b802a5364c90
sanghun9634 <sanghun9634@gmail.com>
2024-06-02 01:24:02 +0900

    feat: change build option

---
+++ deploy.py
@@ -0,0 +1,15 @@
+from prefect import flow
+from prefect.deployments import DeploymentImage
+
+from .flows.flow import hello
+
+
+if __name__ == "__main__":
+    hello.deploy(
+        name="test-prefect-docker-deployment",
+        work_pool_name="docker-agent-pool",
+        work_queue_name="docker-agent",
+        image="test-prefect-docker:0.8",
+        cron="0 * * * *",
+        build=False
+    )
--- flows/flow.py
+++ flows/flow.py
@@ -1,16 +1,9 @@
 from prefect import flow
-from prefect.deployments import DeploymentImage
 
 @flow(log_prints=True)
 def hello():
     print("Hello!")
 
 if __name__ == "__main__":
-    hello.deploy(
-        name="test-prefect-docker-deployment",
-        work_pool_name="docker-agent-pool",
-        work_queue_name="docker-agent",
-        image="test-prefect-docker:0.7",
-        cron="0 * * * *",
-        build=False
-    )
+
+    hello.serve()
