558359415fff5470c149aec06cac52068a28501f
sanghun9634 <sanghun9634@gmail.com>
2024-06-01 23:53:25 +0900

    feat: change build

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.10-slim
+FROM prefecthq/prefect:2.18.3-python3.10
 
-WORKDIR /usr/src/app
-
-COPY . .
+COPY requirements.txt .
 
 RUN python -m pip install --upgrade pip \
     && pip install --no-cache-dir -r requirements.txt
 
-CMD ["prefect", "flow-run", "execute"]
+COPY flows /opt/prefect/flows
+
+WORKDIR /opt/prefect/flows
+
+CMD ["python", "flow.py"]
--- flows/flow.py
+++ flows/flow.py
@@ -1,14 +1,16 @@
 from prefect import flow
 from prefect.deployments import DeploymentImage
 
-from flow import hello
+@flow(log_prints=True)
+def hello():
+    print("Hello!")
 
 if __name__ == "__main__":
     hello.deploy(
         name="test-prefect-docker-deployment",
         work_pool_name="docker-agent-pool",
         work_queue_name="docker-agent",
-        image="test-prefect-docker:0.6",
+        image="test-prefect-docker:0.7",
         cron="0 * * * *",
         build=False
     )
