b764e3e491dd561d376dc3cc5da261e5d6d0e667
sanghun9634 <sanghun9634@gmail.com>
2024-06-02 20:11:27 +0900

    feat: change flow build

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
 FROM prefecthq/prefect:2.18.3-python3.10
 
 # Define build-time variable
-ARG PREFECT_API_URL
+#ARG PREFECT_API_URL
 
-ENV PREFECT_API_URL=$PREFECT_API_URL
+#ENV PREFECT_API_URL=$PREFECT_API_URL
 
 COPY requirements.txt .
 
 RUN python -m pip install --upgrade pip \
     && pip install --no-cache-dir -r requirements.txt
 
-COPY flows /opt/prefect/flows
+COPY . /opt/prefect/flows
 
 WORKDIR /opt/prefect/flows
 
--- flow.py
+++ flow.py
@@ -1,7 +1,9 @@
 from prefect import flow
 from prefect.deployments import DeploymentImage
 
-from flows.flow import hello
+@flow(log_prints=True)
+def hello():
+    print("Hello!")
 
 
 if __name__ == "__main__":
@@ -9,7 +11,7 @@ if __name__ == "__main__":
         name="test-prefect-docker-deployment",
         work_pool_name="docker-agent-pool",
         work_queue_name="docker-agent",
-        image="test-prefect-docker:0.9",
+        image="test-prefect-docker:0.10",
         cron="0 * * * *",
-        build=False
+        build=False,
     )
