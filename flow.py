20abedee487004e7c1c97141fc6fdfdb5eab8a97
sanghun9634 <sanghun9634@gmail.com>
2024-06-02 21:36:34 +0900

    feat: add network mode option

--- flow.py
+++ flow.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
         work_queue_name="docker-agent",
         image="test-prefect-docker:0.10",
         cron="0 * * * *",
+        network_mode='team5'
         build=False,
     )
